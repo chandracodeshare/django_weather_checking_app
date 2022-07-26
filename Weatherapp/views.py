fa2ed88bb2a1f99c4a65225ddfe2827d735caae1
chandracodeshare <meamarchandra9458@gmail.com>
2022-07-26 17:52:56 +0530

    initial commit

---
+++ Weatherapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from matplotlib import units
+import datetime
+import requests
+
+# Create your views here.
+def index(request):
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'Amsterdam'
+    
+
+    appid = '5ea5930c978bce73584a58f6ae4b5de4'
+    URL = 'http://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q':city,'appid':appid,'units':'metric'}
+    r =requests.get(url=URL,params=PARAMS)
+    res = r.json()
+    description = res['weather'][0]['description']
+    icon = res['weather'][0]['icon']
+    temp = res['main']['temp']
+    day = datetime.date.today()
+
+    return render(request,'Weatherapp/index.html',
+    {'description':description,'icon':icon,'temp':temp,'day':day,'city':city})
+
